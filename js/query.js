95c351e59145644c736391e4800265b515fde824
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2017-04-21 03:32:59 +0200

    query qui marche du feu de dieux. A changer cela dit

--- js/query.js
+++ js/query.js
@@ -1,6 +1,6 @@
 const mysql       = require('mysql');
 const dotenv      = require('dotenv');
-const writeInFile  = require('./stories');
+const fs          = require('fs');
 
 dotenv.config({path:'../.env'});
 
@@ -13,78 +13,10 @@ const connection  = mysql.createConnection({
     database    : process.env.MYSQL_DATABASE
 });
 
-const category; //catégorie trouvée par le bot
-const feature; //caractéristique trouvée par le bot
+const category="the"; //catégorie trouvée par le bot
+const feature="bio"; //caractéristique trouvée par le bot
 
-const myQuery = "
-DECLARE @TABLE_NAME VARCHAR(128)
-DECLARE @SCHEMA_NAME VARCHAR(128)
-
------------------------------------------------------------------------
-
--- Set up the name of the table here :
-SET @TABLE_NAME = "+category+"
--- Set up the name of the schema here, or just leave set to 'dbo' :
-SET @SCHEMA_NAME = "+process.env.MYSQL_DATABASE+"
-
------------------------------------------------------------------------
-
-DECLARE @vvc_ColumnName VARCHAR(128)
-DECLARE @vvc_ColumnList VARCHAR(MAX)
-
-IF @SCHEMA_NAME =''
-  BEGIN
-  PRINT 'Error : No schema defined!'
-  RETURN
-  END
-
-IF NOT EXISTS (SELECT * FROM sys.tables T JOIN sys.schemas S
-      ON T.schema_id=S.schema_id
-      WHERE T.Name=@TABLE_NAME AND S.name=@SCHEMA_NAME)
-  BEGIN
-  PRINT 'Error : The table '''+@TABLE_NAME+''' in schema '''+
-      @SCHEMA_NAME+''' does not exist in this database!' 
-  RETURN
- END
-
-DECLARE TableCursor CURSOR FAST_FORWARD FOR
-SELECT   CASE WHEN PATINDEX('% %',C.name) > 0 
-     THEN '['+ C.name +']' 
-     ELSE C.name 
-     END
-FROM     sys.columns C
-JOIN     sys.tables T
-ON       C.object_id  = T.object_id
-JOIN     sys.schemas S
-ON       S.schema_id  = T.schema_id
-WHERE    T.name    = @TABLE_NAME
-AND      S.name    = @SCHEMA_NAME
-ORDER BY column_id
-
-
-SET @vvc_ColumnList=''
-
-OPEN TableCursor
-FETCH NEXT FROM TableCursor INTO @vvc_ColumnName
-
-WHILE @@FETCH_STATUS=0
-  BEGIN
-   SET @vvc_ColumnList = @vvc_ColumnList + @vvc_ColumnName
-
-    -- get the details of the next column
-   FETCH NEXT FROM TableCursor INTO @vvc_ColumnName
-
-  -- add a comma if we are not at the end of the row
-   IF @@FETCH_STATUS=0
-    SET @vvc_ColumnList = @vvc_ColumnList + ','
-   END
-
- CLOSE TableCursor
- DEALLOCATE TableCursor
-
-
-SELECT * FROM "+category+" WHERE "+feature+" in (@vvc_ColumnList)"
-
+const myQuery = 'select designation from '+category+' where designation LIKE \'%'+feature+'%\' or description LIKE \'%'+feature+'%\'';
 
 const getProduct = (connection) => new Promise((resolve, reject) => {
     connection.query({
@@ -95,11 +27,22 @@ const getProduct = (connection) => new Promise((resolve, reject) => {
             reject(error);
             return;
         }
-
         resolve({
             products: results,
             product_number: results.length,
         });
+    });
+});
+
+const saveResults = (data) =>  new Promise((resolve, reject) => {
+    fs.writeFile('test.json', JSON.stringify(data), (err) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+
+        console.log('File wrtitten');
+        resolve();
     });
 });
 
@@ -111,7 +54,8 @@ connection.connect(function(err) {
 
     console.log('connected as id ' + connection.threadId);
 
-    Promise.all(getProduct(connection))
+    Promise.all([getProduct(connection)])
+        .then(saveResults)
         .catch(console.error)
         .then(() => {
             connection.end(function(err) {
