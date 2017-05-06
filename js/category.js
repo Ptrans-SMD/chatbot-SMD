cae3aae1faab6e2cf87ade14630ca12a0f5c51e4
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2017-05-06 20:30:42 +0200

    better category handling

--- js/category.js
+++ js/category.js
@@ -1,16 +1,17 @@
-const handleCategory = (categories, text) => {
+const handleCategory = (categories) => {
+	var queryCategory = [];
 	for (i = 0; i < categories.length; i++) {
 		category = categories[i].value;
 
 		// Test in case the bot isn't trained enough
 		if (categories[i].entities !== undefined) {
 			object = categories[i].entities[category][0].value;
-			text += '[' + category + '] : ' + object;
+			queryCategory.push(category, object);
 		} else {
-			text += '[' + category + ']';
+			queryCategory.push(category);
 		}
 	}
-	return text;
+	return queryCategory;
 };
 
 module.exports = handleCategory;
