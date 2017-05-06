22233a958c2d530ba391c2a371d55cac6cf30054
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2017-05-06 20:30:18 +0200

    [WIP] added query handling + better category and features handling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const writeInFile = require('./js/stories');
 const handleIntent = require('./js/intent');
 const handleCategory = require('./js/category');
 const handleFeature = require('./js/feature');
+const sendQuery = require('./js/queryBuilder');
 const path = require('path');
 
 module.exports = function (bp) {
@@ -24,6 +25,10 @@ module.exports = function (bp) {
 		// Where we write the text we'll be sending
 		let text = '';
 
+		// Where we save the category and the features of the product
+		let queryCategory = [];
+		let queryFeatures = [];
+
 		// Handle intents messages
 		if (event.wit.entities.intent !== undefined) {
 			console.log(' >> nb intents : ', event.wit.entities.intent.length);
@@ -35,20 +40,27 @@ module.exports = function (bp) {
 		if (event.wit.entities.categorie !== undefined) {
 			console.log(' >> nb categories : ', event.wit.entities.categorie.length);
 			const categories = event.wit.entities.categorie;
-			text = handleCategory(categories, text);
+			queryCategory = handleCategory(categories);
+			text += " [Category] : " + queryCategory;
 		}
 
 		// Handle features
 		if (event.wit.entities.feature !== undefined) {
 			console.log(' >> nb features : ', event.wit.entities.feature.length);
 			const features = event.wit.entities.feature;
-			text = handleFeature(features, text);
+			queryFeatures = handleFeature(features);
+			text += " [Features] : " + queryFeatures;
 		}
 
 		// Handle if we got nothing
 		if (text == '') {
 			text += 'Je n\'ai pas compris ce que vous avez essayé de dire.';
 		}
+		// Send the query
+		if (queryCategory !== undefined || queryFeatures !== undefined) {
+			sendQuery(queryCategory, queryFeatures);
+		}
+		console.log("text : " + text);
 		bp.messenger.sendText(sender, text);
 		writeInFile(pathStories, 'Bot - ' + text + '\n');
 	})
