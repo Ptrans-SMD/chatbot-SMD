44d6791983c9886a10224b60624764d31a50bc91
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2017-05-10 19:45:35 +0200

    Fixed MySQL-related problems +[WIP] JSON reading

--- index.js
+++ index.js
@@ -6,79 +6,144 @@ const sendQuery = require('./js/queryBuilder');
 const displayProducts = require('./js/displayProducts');
 const path = require('path');
 const fs = require('fs');
+const mysql = require('mysql');
 
 
 module.exports = function (bp) {
 
 	/*
-	 * middlewares : process incoming and outgoing messages
+	 * Informations de connection à la base de données.
+	 * Les infos de connection sont dans le fichier .env qui est lui même dans le git ignore.
+	 * Cela sert à éviter que les accès à la bdd se retrouvent sur github.
+	 *
 	 */
-	bp.middlewares.load()
 
-	bp.hear({
-		type: 'message'
-	}, (event, next) => {
+	const connection = mysql.createConnection({
+		host: process.env.MYSQL_HOST,
+		port: process.env.MYSQL_PORT,
+		user: process.env.MYSQL_ID,
+		password: process.env.MYSQL_PASSWORD,
+		database: process.env.MYSQL_DATABASE
+	});
 
-		// Setup to write in stories
-		const pathName = './stories/' + event.user.first_name + '_' + event.user.last_name + '.txt';
-		const pathStories = path.resolve(__dirname, pathName);
-		const sender = event.user.id;
-		writeInFile(pathStories, event.user.first_name + ' - ' + event.text + '\n');
-
-		// Where we write the text we'll be sending
-		let text = '';
-
-		// Where we save the category and the features of the product
-		let queryCategory = [];
-		let queryFeatures = [];
-
-		// Products found by the query
-		let products = '';
-
-		// Handle intents messages
-		if (event.wit.entities.intent !== undefined) {
-			console.log(' >> nb intents : ', event.wit.entities.intent.length);
-			const intents = event.wit.entities.intent;
-			text = handleIntent(intents, text);
+	connection.connect(function (err) {
+		if (err) {
+			console.error('error connecting : ' + err.stack);
+			return;
 		}
+		console.log('connected as id ' + connection.threadId);
 
-		// Handle categories
-		if (event.wit.entities.categorie !== undefined) {
-			console.log(' >> nb categories : ', event.wit.entities.categorie.length);
-			const categories = event.wit.entities.categorie;
-			queryCategory = handleCategory(categories);
-			text += " [Category] : " + queryCategory;
-		}
+		/*
+		 * middlewares : process incoming and outgoing messages
+		 */
+		bp.middlewares.load()
 
-		// Handle features
-		if (event.wit.entities.feature !== undefined) {
-			console.log(' >> nb features : ', event.wit.entities.feature.length);
-			const features = event.wit.entities.feature;
-			queryFeatures = handleFeature(features);
-			text += " [Features] : " + queryFeatures;
-		}
+		bp.hear({
+			type: 'message'
+		}, (event, next) => {
 
-		// Handle if we got nothing
-		if (text == '') {
-			text += 'Je n\'ai pas compris ce que vous avez essayé de dire.';
-		}
+			// Setup to write in stories
+			const pathName = './stories/' + event.user.first_name + '_' + event.user.last_name + '.txt';
+			const pathStories = path.resolve(__dirname, pathName);
+			const sender = event.user.id;
+			writeInFile(pathStories, event.user.first_name + ' - ' + event.text + '\n');
 
-		// Send the query
-		if (queryCategory.length > 0 && queryFeatures.length > 0) {
-			console.log("je suis bien dans le if");
-			sendQuery(queryCategory, queryFeatures)
-				.then(() => {
-					fs.readFile(path.resolve(__dirname,'./js/test.json'), (err, data) => {
-						if (err) throw err;
-						products = JSON.parse(data);
-						text = displayProducts(products);
-						console.log("ok!");
-						console.log(text);
+			// Where we write the text we'll be sending
+			let text = '';
+
+			// Where we save the category and the features of the product
+			let queryCategory = [];
+			let queryFeatures = [];
+
+			// Products found by the query
+			let products = '';
+
+			// Handle intents messages
+			if (event.wit.entities.intent !== undefined) {
+				console.log(' >> nb intents : ', event.wit.entities.intent.length);
+				const intents = event.wit.entities.intent;
+				text = handleIntent(intents, text);
+			}
+
+			// Handle categories
+			if (event.wit.entities.categorie !== undefined) {
+				console.log(' >> nb categories : ', event.wit.entities.categorie.length);
+				const categories = event.wit.entities.categorie;
+				queryCategory = handleCategory(categories);
+				// text += " [Category] : " + queryCategory;
+			}
+
+			// Handle features
+			if (event.wit.entities.feature !== undefined) {
+				console.log(' >> nb features : ', event.wit.entities.feature.length);
+				const features = event.wit.entities.feature;
+				queryFeatures = handleFeature(features);
+				// text += " [Features] : " + queryFeatures;
+			}
+
+			// Handle if we got nothing
+			if (!Object.keys(event.wit.entities).length) {
+				text += 'Je n\'ai pas compris ce que vous avez essayé de dire.';
+			}
+
+			// Send the query
+			if (queryCategory.length > 0 && queryFeatures.length > 0) {
+				console.log("je suis bien dans le if");
+				Promise.all([sendQuery(connection, queryCategory, queryFeatures)])
+					.then(() => new Promise((resolve, reject) => {
+						fs.readFile(path.resolve(__dirname, './js/test.json'), (err, data) => {
+							if (err) throw err;
+							products = JSON.parse(data);
+							text = displayProducts(products);
+							console.log("ok!");
+							console.log(text);
+							resolve();
+						});
+					}))
+					.then(() => {
+						console.log('console log de génie : ' + text);
+						bp.messenger.sendText(sender, text);
 					});
+			} else {
+				console.log(text);
+				bp.messenger.sendText(sender, text);
+			}
+
+			writeInFile(pathStories, 'Bot - ' + text + '\n');
+		})
+
+		if (process.platform === "win32") {
+			var rl = require("readline").createInterface({
+				input: process.stdin,
+				output: process.stdout
+			});
+
+			rl.on("SIGINT", function () {
+				console.log("SIGINT revceived: Terminating connection to the db");
+				connection.end(function (err) {
+					if (err) {
+						console.log(err);
+						process.exit();
+					}
+
+					console.log('The connection is terminated now');
+					process.exit();
 				});
+			});
 		}
-		console.log("text : " + text);
-		bp.messenger.sendText(sender, text);
-		writeInFile(pathStories, 'Bot - ' + text + '\n');
-	})
+
+		process.on("SIGINT", function () {
+			console.log("SIGINT revceived: Terminating connection to the db");
+			connection.end(function (err) {
+				if (err) {
+					console.log(err);
+					process.exit();
+				}
+
+				console.log('The connection is terminated now');
+				//graceful shutdown
+				process.exit();
+			});
+		});
+	});
 }
