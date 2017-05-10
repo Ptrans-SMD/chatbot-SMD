080d1494b30186406c60f1c98726b6544ecb7a29
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2017-05-10 19:45:51 +0200

    Fixed MySQL-related problems

--- js/queryBuilder.js
+++ js/queryBuilder.js
@@ -7,24 +7,6 @@ dotenv.config({
     path: path.resolve(__dirname, '../.env')
 });
 
-/*
- * Informations de connection à la base de données.
- * Les infos de connection sont dans le fichier .env qui est lui même dans le git ignore.
- * Cela sert à éviter que les accès à la bdd se retrouvent sur github.
- *
- */
-
-const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_ID,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-});
-
-console.log(process.env.MYSQL_HOST);
-
-
 const saveResults = (data) => new Promise((resolve, reject) => {
     fs.writeFile(path.resolve(__dirname, './test.json'), JSON.stringify(data), (err) => {
         if (err) {
@@ -53,7 +35,7 @@ const getProduct = (connection, myQuery) => new Promise((resolve, reject) => {
     });
 });
 
-const sendQuery = (category, feature) => new Promise((resolve, reject) => {
+const sendQuery = (connection, category, feature) => new Promise((resolve, reject) => {
 
     var subQuery = 'select designation from ' + category[0] + ' where (designation LIKE \'%' + category[1] + '%\' or description LIKE \'%' + category[1] + '%\') AND';
     var subQuery2 = 'select designation from ' + category[0] + ' where (designation LIKE \'%' + category[1] + '%\') AND';
@@ -70,29 +52,13 @@ const sendQuery = (category, feature) => new Promise((resolve, reject) => {
 
     const myQuery = 'CALL LoadProduct(\"' + category + '\",\"' + subQuery + '\",\"' + subQuery2 + '\")';
     console.log(myQuery);
-    connection.connect(function (err) {
-        if (err) {
-            console.error('error connecting : ' + err.stack);
-            return;
-        }
 
-        console.log('connected as id ' + connection.threadId);
-
-        Promise.all([getProduct(connection, myQuery)])
-            .then(saveResults)
-            .catch(reject)
-            .then(() => {
-                connection.end(function (err) {
-                    if (err) {
-                        reject(err);
-                        return;
-                    }
-
-                    console.log('The connection is terminated now');
-                    resolve();
-                });
-            });
-    });
+    Promise.all([getProduct(connection, myQuery)])
+        .then(saveResults)
+        .catch(reject)
+        .then(()=>{
+            resolve();
+        });
 });
 
 module.exports = sendQuery;
