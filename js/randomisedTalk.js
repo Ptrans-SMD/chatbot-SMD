f3665e0e7ff5947e2a8b312061299188af50959c
Thomas Coquereau <thomas.coquereau@etu.univ-nantes.fr>
2017-05-11 09:14:34 +0200

    Meilleur exemple de phrase

--- js/randomisedTalk.js
+++ js/randomisedTalk.js
@@ -12,7 +12,7 @@ const greetings = [
 	`Bonjour bonjour, ici Sébastien !`,
 	`Bon, il semble que votre recherche n'ai rien donné, mais qu'à cela ne tienne, je suis là pour vous aider !`,
 	`Pour ça, vous n'avez qu'à me dire ce que vous voulez, naturellement.`,
-	`Par exemple vous pouvez me demander une phrase du genre "Je veux une table en pin, d'extérieur" et si tout se passe bien je trouverais votre bonheur !`,
+	`Par exemple vous pouvez me demander une phrase du genre "Je veux du thé bio au jasmin" et si tout se passe bien je trouverais votre bonheur !`,
 	`Si vous voulez de nouveau des explications au cours de notre conversation, n'hésitez pas, et demandez moi simplement de l'aide !`
 ];
 const aide = [
@@ -20,7 +20,7 @@ const aide = [
 	`Alors, pour rappel : Je suis là pour vous aider à reformuler votre demande.`,
 	`En fait, dans l'idéal, il faudrait que vous me donniez un type de produit et des caractéristiques voulues.`,
 	`Par exemple, envoyez moi une phrase de la forme :`,
-	`"Je veux une table en pin d\'exterieur."`
+	`"Je veux du thé bio au jasmin."`
 ];
 const thanks = [
 	['De rien.'],
